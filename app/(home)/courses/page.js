3de577b194b8ba0ac7e72981441de4d0875ab21a
newazbenalam <newazbenalam@gmail.com>
2024-08-14 01:32:29 +0600

    courses list

--- app/(home)/courses/page.js
+++ app/(home)/courses/page.js
@@ -1,10 +1,39 @@
 
 "use client";
 
+import { GetCourses } from "@/app/_lib/actions/CoursesUsecase";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function CoursesList() {
+  const [courses, setCourses] = React.useState([]);
+
+  useEffect(() => { 
+
+    // fetch api
+    // const res = fetch("http://localhost:3000/api/courses");
+    // res.then((data) => {
+    //   data.json().then((data) => {
+    //     console.log(data);
+    //   });
+
+
+    // // Server Action
+    // GetCourses().then((data) => {
+    //   setCourses(data);
+    //   console.log(courses);
+    // });
+    
+
+    const getData  = async () => {
+      const res = await GetCourses();
+      setCourses(res);
+      console.log(res);
+    }
+
+    getData();
+
+  }, []);
 
 
 
@@ -44,6 +73,8 @@ export default function CoursesList() {
             </li>
           </ul>
           <div className="row event_box">
+
+
             <div className="col-lg-4 col-md-6 align-self-center mb-30 event_outer col-md-6 design">
               <div className="events_item">
                 <div className="thumb">
@@ -63,106 +94,34 @@ export default function CoursesList() {
                 </div>
               </div>
             </div>
-            <div className="col-lg-4 col-md-6 align-self-center mb-30 event_outer col-md-6  development">
-              <div className="events_item">
-                <div className="thumb">
-                  <a href="#">
-                    <img src="assets/images/course-02.jpg" alt="" />
-                  </a>
-                  <span className="category">Development</span>
-                  <span className="price">
-                    <h6>
-                      <em>$</em>340
-                    </h6>
-                  </span>
+
+            {courses.map((course) => (
+              <div key={course.id} className="col-lg-4 col-md-6 align-self-center mb-30 event_outer col-md-6 design">
+                  <Link href={`/courses/${course.id}`} >
+                  <div className="events_item">
+                    <div className="thumb">
+                      <a href="#">
+                        <img src="assets/images/course-01.jpg" alt="" />
+                      </a>
+                      <span className="category">{course.category.title}</span>
+                      <span className="price">
+                        <h6>
+                          <em>$</em>{course.price}
+                        </h6>
+                      </span>
+                    </div>
+                    <div className="down-content">
+                      <span className="author">{course.instructor.name}</span>
+                      <h4>{course.title}</h4>
+                    </div>
+                  </div>
+              </Link>
                 </div>
-                <div className="down-content">
-                  <span className="author">Cindy Walker</span>
-                  <h4>Web Development Tips</h4>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6 align-self-center mb-30 event_outer col-md-6 design wordpress">
-              <div className="events_item">
-                <div className="thumb">
-                  <a href="#">
-                    <img src="assets/images/course-03.jpg" alt="" />
-                  </a>
-                  <span className="category">Wordpress</span>
-                  <span className="price">
-                    <h6>
-                      <em>$</em>640
-                    </h6>
-                  </span>
-                </div>
-                <div className="down-content">
-                  <span className="author">David Hutson</span>
-                  <h4>Latest Web Trends</h4>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6 align-self-center mb-30 event_outer col-md-6 development">
-              <div className="events_item">
-                <div className="thumb">
-                  <Link href='/courses/11'>
-                    <img src="assets/images/course-04.jpg" alt="" />
-                  </Link>
-                  <span className="category">Development</span>
-                  <span className="price">
-                    <h6>
-                      <em>$</em>450
-                    </h6>
-                  </span>
-                </div>
-                <div className="down-content">
-                  <span className="author">Stella Blair</span>
-                  <h4>Online Learning Steps</h4>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6 align-self-center mb-30 event_outer col-md-6 wordpress development">
-              <div className="events_item">
-                <div className="thumb">
-                  <a href="#">
-                    <img
-                      height={310}
-                      style={{ objectFit: "cover" }}
-                      src="assets/1713299014367.jpeg"
-                      alt=""
-                    />
-                  </a>
-                  <span className="category">Wordpress</span>
-                  <span className="price">
-                    <h6>
-                      <em>$</em>320
-                    </h6>
-                  </span>
-                </div>
-                <div className="down-content">
-                  <span className="author">Sophia Rose</span>
-                  <h4>Be a WordPress Master</h4>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6 align-self-center mb-30 event_outer col-md-6 wordpress design">
-              <div className="events_item">
-                <div className="thumb">
-                  <a href="#">
-                    <img src="assets/images/course-06.jpg" alt="" />
-                  </a>
-                  <span className="category">Webdesign</span>
-                  <span className="price">
-                    <h6>
-                      <em>$</em>240
-                    </h6>
-                  </span>
-                </div>
-                <div className="down-content">
-                  <span className="author">David Hutson</span>
-                  <h4>Full Stack Developer</h4>
-                </div>
-              </div>
-            </div>
+
+            ))}
+            
+
+            
           </div>
         </div>
       </section>
