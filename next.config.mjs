df7b7ef693b43f779002a075b3d1668eb286b321
newazbenalam <newazbenalam@gmail.com>
2024-09-19 20:59:37 +0600

    Fix: crossOrgin

--- next.config.mjs
+++ next.config.mjs
@@ -24,7 +24,33 @@ const nextConfig = {
       },
     ],
   },
-  reactStrictMode: false
+  poweredByHeader: false,
+  cleanDistDir: true,
+  crossOrigin: 'anonymous',
+  env: {
+    HOSTNAME: 'localhost',
+  },
+  logging: {
+    level: 'info',
+  },
+  reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ];
+  },
 
 };
 
